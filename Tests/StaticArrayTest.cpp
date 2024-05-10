2c624cafe795f6b876e2797056d1a62f0421e9b5
aaron <aaron.t.hamil@ou.edu>
2024-05-10 15:18:27 -0500

    StaticArray passed all current Google Tests verifying implementation of current data structure model, will come back to add increased functionality such as copy, move, and iterators.

--- Tests/StaticArrayTest.cpp
+++ Tests/StaticArrayTest.cpp
@@ -24,3 +24,58 @@ TEST(StaticArrayTest, AddedElementsPersist) {
     EXPECT_EQ(array[1], 20);
     EXPECT_EQ(array.size(), 10);
 }
+
+TEST(StaticArrayTest, OutOfBoundException) {
+    StaticArray<int, 10> array{};
+    EXPECT_THROW(array.at(10), std::out_of_range);
+}
+
+TEST(StaticArrayTest, CheckFrontAndBack) {
+    StaticArray<int, 10> array{};
+    array[0] = 10;
+    array[9] = 20;
+    EXPECT_EQ(array.front(), 10);
+    EXPECT_EQ(array.back(), 20);
+}
+
+TEST(StaticArrayTest, CheckEmpty) {
+    StaticArray<int, 0> array{};
+    EXPECT_TRUE(array.empty());
+}
+
+TEST(StaticArrayTest, CheckFill) {
+    StaticArray<int, 10> array{};
+    array.fill(5);
+    for (int i : array) {
+        EXPECT_EQ(i, 5);
+    }
+}
+
+TEST(StaticArrayTest, CheckSwap) {
+    StaticArray<int, 3> array1{};
+    array1[0] = 1;
+    array1[1] = 2;
+    array1[2] = 3;
+
+    StaticArray<int, 3> array2{};
+    array2[0] = 4;
+    array2[1] = 5;
+    array2[2] = 6;
+
+    EXPECT_EQ(array1[0], 1);
+    EXPECT_EQ(array1[1], 2);
+    EXPECT_EQ(array1[2], 3);
+    EXPECT_EQ(array2[0], 4);
+    EXPECT_EQ(array2[1], 5);
+    EXPECT_EQ(array2[2], 6);
+
+    array1.swap(array2);
+
+    EXPECT_EQ(array1[0], 4);
+    EXPECT_EQ(array1[1], 5);
+    EXPECT_EQ(array1[2], 6);
+    EXPECT_EQ(array2[0], 1);
+    EXPECT_EQ(array2[1], 2);
+    EXPECT_EQ(array2[2], 3);
+
+}
