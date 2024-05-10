918382bb532bafb99c516da42e45be05cb7602c6
aaron <aaron.t.hamil@ou.edu>
2024-05-10 14:49:52 -0500

    Fixed Testing configuration as submodule

--- Tests/StaticArrayTest.cpp
+++ Tests/StaticArrayTest.cpp
@@ -1,6 +1,9 @@
 #include <gtest/gtest.h>
 #include "../Structures/Static/StaticArray.h"
 
+// You want to go into the cmake-build-debug directory and run ./Google_Test_run
+// Make sure you rebuild/recompile after changing.
+
 TEST(StaticArrayTest, SizeOfEmptyArrayShouldBeZero) {
     StaticArray<int, 10> array{};
     EXPECT_EQ(array.size(), 0);
