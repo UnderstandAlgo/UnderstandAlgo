75ec16236eae6d60ccf9d7af9647fb3cdc8dcf99
Kyumin Lee <2km4890@gmail.com>
2024-05-18 23:36:42 -0500

    redesigning frontend

--- script.js
+++ script.js
@@ -1,29 +1,52 @@
-
-
-
-
 const DSbtn = document.getElementById('DSbtn');
 const Abtn = document.getElementById('Abtn');
-
 const menu = document.getElementById('menu');
-
+const codeBox = document.getElementById('code-box');
 
 const DScontent = `
 <ul>
-    <li><a href="url">Array</a></li>
-    <li><a href="url">Stack</a></li>
-    <li><a href="url">Tree</a></li>
+    <li>
+        <button onclick="toggleSubmenu('arraySubmenu')">Array</button>
+        <ul id="arraySubmenu" class="submenu">
+            <li><button onclick="loadCode('codes/array_cpp.txt')">C++</button></li>
+            <li><button onclick="loadCode('codes/array_c.txt')">C</button></li>
+        </ul>
+    </li>
+    <li>
+        <button onclick="toggleSubmenu('stackSubmenu')">Stack</button>
+        <ul id="stackSubmenu" class="submenu">
+            <li><button onclick="loadCode('codes/stack_cpp.txt')">C++</button></li>
+            <li><button onclick="loadCode('codes/stack_c.txt')">C</button></li>
+        </ul>
+    </li>
+    <li>
+        <button onclick="toggleSubmenu('treeSubmenu')">Tree</button>
+        <ul id="treeSubmenu" class="submenu">
+            <li><button onclick="loadCode('codes/tree_cpp.txt')">C++</button></li>
+            <li><button onclick="loadCode('codes/tree_c.txt')">C</button></li>
+        </ul>
+    </li>
 </ul>
 `;
 
 const Acontent = `
 <ul>
-    <li><a href="url">Searching</a></li>
-    <li><a href="url">Sorting</a></li>
+    <li>
+        <button onclick="toggleSubmenu('searchingSubmenu')">Searching</button>
+        <ul id="searchingSubmenu" class="submenu">
+            <li><button onclick="loadCode('codes/bfs_cpp.txt')">BFS - C++</button></li>
+            <li><button onclick="loadCode('codes/bfs_c.txt')">BFS - C</button></li>
+        </ul>
+    </li>
+    <li>
+        <button onclick="toggleSubmenu('sortingSubmenu')">Sorting</button>
+        <ul id="sortingSubmenu" class="submenu">
+            <li><button onclick="loadCode('codes/sorting_cpp.txt')">C++</button></li>
+            <li><button onclick="loadCode('codes/sorting_c.txt')">C</button></li>
+        </ul>
+    </li>
 </ul>
 `;
-
-
 
 let showDS = false;
 let showA = false;
@@ -50,3 +73,19 @@ function updateMenu() {
     }
 }
 
+function toggleSubmenu(id) {
+    const submenu = document.getElementById(id);
+    submenu.style.display = submenu.style.display === 'none' ? 'block' : 'none';
+}
+
+function loadCode(filePath) {
+    fetch(filePath)
+        .then(response => response.text())
+        .then(data => {
+            codeBox.value = data;
+        })
+        .catch(error => {
+            codeBox.value = 'Error loading code.';
+            console.error('Error:', error);
+        });
+}
