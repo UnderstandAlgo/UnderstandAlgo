ac181ce3c7357316d7a0332b23a8279d6217b0aa
aaron <aaron.t.hamil@ou.edu>
2024-05-28 16:09:27 -0500

    Update Graph class methods with `[[maybe_unused]]` and `[[nodiscard]]` attributes
    The Graph class methods have been updated to include `[[maybe_unused]]` attribute to avoid compiler warnings about unused parameters. Additionally, for methods returning useful information such as `numVertices` and `numEdges`, `[[nodiscard]]` attribute has been added to ensure return values are not unintentionally ignored. Also, minor log changes were applied correcting test start and end times.

--- Structures/ADT/Graph.hpp
+++ Structures/ADT/Graph.hpp
@@ -15,7 +15,7 @@ public:
     *
     * @param vertex The vertex to be added.
     */
-    virtual void addVertex(const VerticeType& vertex) = 0;
+    [[maybe_unused]] virtual void addVertex(const VerticeType& vertex) = 0;
 
     /**
     * @brief Remove a vertex from the graph.
@@ -25,7 +25,7 @@ public:
     *
     * @param vertex The vertex to be removed.
     */
-    virtual void removeVertex(const VerticeType& vertex) = 0;
+    [[maybe_unused]] virtual void removeVertex(const VerticeType& vertex) = 0;
 
     /**
     * @brief Adds an edge to the graph.
@@ -38,7 +38,7 @@ public:
     * @param destination The destination vertex where the edge should end.
     * @param weight The weight of the edge.
     */
-    virtual void addEdge(const VerticeType &source, const VerticeType &destination, const EdgeType &weight) = 0;
+    [[maybe_unused]] virtual void addEdge(const VerticeType &source, const VerticeType &destination, const EdgeType &weight) = 0;
 
     /**
     * @brief Adds an edge between two vertices. When the graph is directed,
@@ -54,7 +54,7 @@ public:
     * @param isDirected (optional) Flag indicating if the graph is directed. If true, the edge is directed from source to destination.
     * Default is false, which means the edge is bidirectional.
     */
-    virtual void addEdge(const VerticeType& source, const VerticeType& destination,
+    [[maybe_unused]] virtual void addEdge(const VerticeType& source, const VerticeType& destination,
                          const EdgeType& weight, bool isDirected = false) = 0;
 
     /**
@@ -66,7 +66,7 @@ public:
     * @param vertex1 The first vertex.
     * @param vertex2 The second vertex.
     */
-    virtual void removeEdge(const VerticeType& vertex1, const VerticeType& vertex2) = 0;
+    [[maybe_unused]] virtual void removeEdge(const VerticeType& vertex1, const VerticeType& vertex2) = 0;
 
     /**
     * @brief Gets the total number of vertices in the graph.
@@ -76,7 +76,7 @@ public:
     *
     * @return unsigned int The total number of vertices.
     */
-    virtual unsigned int numVertices() const = 0;
+    [[maybe_unused]] [[nodiscard]] virtual unsigned int numVertices() const = 0;
 
     /**
     * @brief Gets the total number of edges in the graph.
@@ -86,7 +86,7 @@ public:
     *
     * @return unsigned int The total number of edges.
     */
-    virtual unsigned int numEdges() const = 0;
+    [[maybe_unused]] [[nodiscard]] virtual unsigned int numEdges() const = 0;
 };
 
 template<typename VerticeType, typename EdgeType>
@@ -101,8 +101,8 @@ public:
     void addEdge(const VerticeType &source, const VerticeType &destination, const EdgeType &weight) override; // Overridden addEdge() function
     void addEdge(const VerticeType &source, const VerticeType &destination, const EdgeType &weight, bool isDirected) override; // Overridden addEdge() function with additional isDirected parameter
     void removeEdge(const VerticeType &vertex1, const VerticeType &vertex2) override; // Overridden removeEdge() function
-    unsigned int numVertices() const override; // Overridden numVertices() function
-    unsigned int numEdges() const override; // Overridden numEdges() function
+    [[nodiscard]] unsigned int numVertices() const override; // Overridden numVertices() function
+    [[nodiscard]] unsigned int numEdges() const override; // Overridden numEdges() function
 
 };
 #include "Graph.tpp"
